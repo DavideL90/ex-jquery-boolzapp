8c63903d6785d108d8881fe518729fbb67d62e81
DavideL90 <davide.lecci@icloud.com>
2018-04-10 14:07:19 +0200

    automatic answer implemented

--- main.js
+++ main.js
@@ -9,12 +9,22 @@ $(document).ready(function(){
   $('.fa-paper-plane').click(function(){
     var input = $('#msg-input').val();
     $('#msg-input').val("");
+    //ho messo la condizione perché se scrivo qualcosa
+    //e poi la cancello e clicco allora mando un mess vuoto
     if(input != ""){
       var row = $('<div>' + '</div>').addClass('row');
       var lastDiv = $('<div>' + '</div>').addClass('mess sent').html(input);
-      $('#messages').append(row.append(lastDiv));
+      var arrow = $('<div>' + '</div>').addClass('arrow-down');
+      $('#messages').append(row.append(lastDiv.append(arrow)));
     }
     $('.fa-microphone').show();
     $('.fa-paper-plane').hide();
+    setTimeout(function(){
+      row = $('<div>' + '</div>').addClass('row');
+      lastDiv = $('<div>' + '</div>').addClass('mess received').html("Ok!");
+      arrow = $('<div>' + '</div>').addClass('arrow-down');
+      $('#messages').append(row.append(lastDiv.append(arrow)));
+    }, 1000);
   });
+  //Quando scrivo sulla barra viene filtrata la lista
 });
